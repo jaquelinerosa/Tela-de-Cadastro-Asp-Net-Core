92f333eb078814820e52c9a0e6b43ce56dbd910e
JAQUELINE ROSA <djjrosa@hotmail.com>
2022-05-27 21:05:05 -0300

    Adicionar arquivos de projeto.

---
+++ Xadrez/Controllers/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xadrez.Controllers
+{
+    public class Usuario : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult  Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+
+    }
+}
